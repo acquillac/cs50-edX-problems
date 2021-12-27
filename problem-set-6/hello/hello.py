c3ff4490ae823476a50e0b5d2c2da3c80542cff0
Acquilla <90166904+acquillac@users.noreply.github.com>
2021-12-26 19:55:49 -0700

    pset6

---
+++ problem-set-6/hello/hello.py
@@ -0,0 +1,6 @@
+from cs50 import get_string
+
+# Ask user for name
+name = get_string("What is your name? \n")
+# Print hello, name
+print(f"hello, {name}")
---
+++ problem-set-6/mario/more/mario.py
@@ -0,0 +1,16 @@
+from cs50 import get_int
+
+# Get height from user
+height = get_int("Height: ")
+
+# Check for valid height
+while (True):
+    if (height > 8 or height < 1):
+        height = get_int("Height: ")
+    else:
+        break
+
+# Print pyramid
+for i in range(height):
+    print((height - 1 - i) * " ", end="")
+    print((1 + i) * "#" + "  " + (1 + i) * "#")
